579f6bd1a92b9d6ece894994c762ebe276dac894
Heitor Rodrigues <heitor.vrb@gmail.com>
2026-04-03 21:17:31 -0300

    Use dark_lite to fix top bar gtk theming

--- crates/app/src/main.rs
+++ crates/app/src/main.rs
@@ -1,5 +1,22 @@
 mod app;
 
+use dioxus::desktop::{Config, WindowBuilder};
+use dioxus::desktop::tao::window::Theme;
+use dioxus::prelude::*;
+
 fn main() {
-    dioxus::launch(app::App);
+    let theme = match dark_light::detect() {
+        Ok(dark_light::Mode::Dark) => Theme::Dark,
+        _ => Theme::Light,
+    };
+
+    LaunchBuilder::desktop()
+        .with_cfg(
+            Config::new().with_window(
+                WindowBuilder::new()
+                    .with_title("Kitesurf")
+                    .with_theme(Some(theme)),
+            ),
+        )
+        .launch(app::App);
 }
